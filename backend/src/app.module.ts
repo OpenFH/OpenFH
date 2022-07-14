814c73656ee3485df7656ef48643b6b72b8741cf
Benjamin E <benjamin.ehnes@fh-erfurt.de>
2022-07-14 19:30:42 +0200

    OP-1:added Redis as the main-cache

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,9 @@
 import { join } from 'path';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { Module } from '@nestjs/common';
+import { Module, CacheModule } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import * as Redis from 'cache-manager-redis-store';
 import { PersonModule } from './features/person/person.module';
 import { PrismaService } from './features/prisma/prisma.service';
 
@@ -11,6 +13,17 @@ import { PrismaService } from './features/prisma/prisma.service';
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/types/schema.gql'),
       sortSchema: true,
+    }),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        store: Redis,
+        host: configService.get('REDIS_HOST'),
+        port: configService.get('REDIS_PORT'),
+        ttl: configService.get('REDIS_TTL'),
+        auth_pass: configService.get('REDIS_PW'),
+      }),
     }),
     PersonModule,
   ],
