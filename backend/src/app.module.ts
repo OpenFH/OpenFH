52c1484fb872ed74890f208e8c742ccdd0e45cd6
Benjamin E <benjamin.ehnes@fh-erfurt.de>
2022-07-14 20:14:37 +0200

    OP-1:cleaned up scripts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,32 +1,11 @@
-import { join } from 'path';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { Module, CacheModule } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
-import * as Redis from 'cache-manager-redis-store';
+import { Module } from '@nestjs/common';
+import { GraphQlModule } from './config/graphql';
+import { CacheModule } from './config/redis';
 import { PersonModule } from './features/person/person.module';
 import { PrismaService } from './features/prisma/prisma.service';
 
 @Module({
-  imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/types/schema.gql'),
-      sortSchema: true,
-    }),
-    CacheModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        store: Redis,
-        host: configService.get('REDIS_HOST'),
-        port: configService.get('REDIS_PORT'),
-        ttl: configService.get('REDIS_TTL'),
-        auth_pass: configService.get('REDIS_PW'),
-      }),
-    }),
-    PersonModule,
-  ],
+  imports: [CacheModule, GraphQlModule, PersonModule],
   providers: [PrismaService],
 })
 export class AppModule {}
