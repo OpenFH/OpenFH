c1ecfd455fc55b62fc34420ed1c1bfa253fd5453
Benjamin E <benjamin.ehnes@fh-erfurt.de>
2022-07-15 13:29:48 +0200

    Added example configs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter(),
   );
-  await app.listen(process.env.PORT);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
